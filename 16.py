d6d842eef2aab62fad073a708362023f8775cab9
Majid Khosravi <me@majidkhosravi.com>
2024-12-16 20:04:29 +0000

    Day 16 part 2

--- 16.py
+++ 16.py
@@ -1,4 +1,5 @@
-import sys,functools,copy
+import sys,copy
+
 f = open("inputs/16.txt", "r")
 lines = f.read().splitlines()
 f.close()
@@ -17,7 +18,6 @@ def print_map( map_ ):
 
     for i in range(width):
         for j in range(height):
-
             output += f"{str(map_[i][j]):4}"
 
         output += "\n"
@@ -62,13 +62,16 @@ def rotate ( dir_ , degree ):
 
     return None
 
-def get_neighbours( cursor ):
+def get_neighbours( cursor , current_dir ):
 
     neighbours = []
 
-    for i in directions:
-        if 0 <= cursor[0] + i[0] < width and 0 <= cursor[1] + i[1] < height:
-            neighbours.append( (cursor[0] + i[0], cursor[1] + i[1]) )
+    left = rotate(current_dir , - 90)
+    right = rotate(current_dir , 90)
+
+    neighbours.append( (cursor[0] + current_dir[0], cursor[1] + current_dir[1]) )
+    neighbours.append( (cursor[0] + left[0], cursor[1] + left[1]) )
+    neighbours.append( (cursor[0] + right[0], cursor[1] + right[1]) )
 
     return neighbours
 
@@ -83,28 +86,46 @@ def get_direction ( prev_pos , next_pos ):
     elif prev_pos[1] > next_pos[1]:
         return directions[3]
 
+def get_seat_count (input_map ):
+    count = 0
+    for i in range(width):
+        for j in range(height):
+            if input_map[i][j] == "O":
+                count += 1
+    return count
+
 start_pos = find_symbol('S')
 end_pos = find_symbol('E')
-
 best_path = {}
 
+paths = []
+
 # Part 1
-def find_path ( cursor , end , current_dir , cost , min_cost ):
+def find_path ( cursor , end , current_dir , cost , min_cost , path ):
 
     if cursor == end:
+        if min_cost > 0 or cost < min_cost:
+            paths.append( [ cost , path] )
         return cost
 
-    state = (cursor[0] , cursor[1] )
-    if state in best_path and best_path[state] <= cost:
+    state = (cursor[0] , cursor[1] , current_dir )
+
+    if cost > min_cost > 0:
+        return None
+
+    if state in best_path and best_path[state] < cost:
         return None
 
     best_path[state] = cost
 
-    neighbours = get_neighbours( cursor )
+    neighbours = get_neighbours( cursor , current_dir )
     for n in neighbours:
         item = get_item(n)
 
         if item == "#":
+            continue
+
+        if n in path:
             continue
 
         if item in [".","E"]:
@@ -117,11 +138,20 @@ def find_path ( cursor , end , current_dir , cost , min_cost ):
             if new_dir != current_dir:
                 addition += 1000
 
-            c = find_path( n  , end  , new_dir, cost + addition , min_cost )
+            c = find_path( n  , end  , new_dir, cost + addition , min_cost , path + [(n[0],n[1])] )
+
             if c is not None and c < min_cost:
                 min_cost = c
 
     return min_cost
 
-result = find_path( start_pos , end_pos , direction,  0 , float("inf")  )
-print(result)
+lowest_score = find_path( start_pos , end_pos , direction,  0 , float("inf") ,   [(start_pos[0] , start_pos[1])]  )
+print("part1", lowest_score)
+
+# part 2
+for result in paths:
+    if result[0] == lowest_score:
+        for p in result[1]:
+            grid2[p[0]][p[1]] = "O"
+
+print("part 2", get_seat_count(grid2))
