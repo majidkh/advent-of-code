43ff0fd9cf1a76c74998428f33f336d524528404
Majid Khosravi <me@majidkhosravi.com>
2024-12-19 06:14:02 +0000

    part2 added, needs optimizing

--- 19.py
+++ 19.py
@@ -8,28 +8,30 @@ patterns = [pattern.strip() for pattern in patterns]
 
 designs = lines[1].strip().split("\n")
 
-
-seens = []
-def is_possible ( design_ , options_ , unique ):
+def is_possible ( design_ , options_  ):
 
     if len(design_) == 0:
         options_ += 1
-        return options_
 
+        return options_
 
     for p in patterns:
         if design_.startswith(p):
-            options_ = is_possible ( design_[len(p):] , options_ , unique )
-
-            if unique and options_ > 0 :
-                return 1
+            options_ = is_possible ( design_[len(p):] , options_)
 
     return options_
 
-part1 = 0
-for design in designs:
-    options = is_possible( design , 0 , True )
 
-    part1 += options
+
+part1 = 0
+part2 = 0
+for design in designs:
+    options = is_possible( design , 0  )
+
+    if options > 0:
+        part1 += 1
+
+    part2 += options
 
 print("Part 1:", part1)
+print("Part 2:", part2)
