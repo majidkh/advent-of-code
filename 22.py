47ed9351c85c58d32138ed345bc9a8b0fb982e33
Majid Khosravi <me@majidkhosravi.com>
2024-12-22 12:48:23 +0000

    day 22 code cleanup

--- 22.py
+++ 22.py
@@ -1,102 +1,81 @@
-from collections import deque
 from functools import cache
 
 f = open("inputs/22.txt", "r")
 numbers = tuple(map(int,f.read().splitlines()))
 f.close()
 
-@cache
+numbers_map = {}
+
 def evolve ( in_number ):
 
-    val = in_number * 64
-    in_number = val ^ in_number # Mix
+    value = in_number * 64
+    in_number = value ^ in_number # Mix
     in_number = int(in_number % 16777216) # Prune
 
-    val = in_number // 32
-    in_number = val ^ in_number
+    value = in_number // 32
+    in_number = value ^ in_number
     in_number = int(in_number % 16777216) # Prune
 
-    val = in_number * 2048
-    in_number = val ^ in_number
+    value = in_number * 2048
+    in_number = value ^ in_number
     in_number = int(in_number % 16777216) # Prune
     return in_number
 
+sequence_map = {}
+unique_sequences = set()
+
 part1 = 0
 for number in numbers:
+    val = number
+    prev_price = val % 10
+    numbers_changes = {}
+    best_prices = {}
+
     for i in range(2000):
-        number = evolve(number)
-    part1 += number
+        val = evolve(val)
+        price = val % 10
+        change = price - prev_price
+
+        numbers_changes[i] = change
+        prev_price = price
+
+        # Calculate the sequence starting on the 4th item ( 0 , 1 , 2 , 3 ...)
+        if i > 2:
+
+            sequence = ''
+            for j in range( i - 3 , i + 1 ):
+                sequence += str(numbers_changes[j]) + ' '
+            sequence = sequence.strip()
+
+            numbers_map[str(number) + "_" + str(i)] = [sequence,price]
+
+            if sequence not in best_prices:
+                best_prices[sequence] = price
+            else:
+                if best_prices[sequence] < price:
+                    best_prices[sequence] = price
+
+            unique_sequences.add(sequence)
+
+    sequence_map[number] = best_prices
+
+    part1 += val
 
 print("Part 1:", part1)
 
-@cache
-def get_offer ( secrets, sequence ):
-    result = []
-
-    for secret in secrets:
-        pointer = 0
-        prev_price = secret % 10
-
-        offers = []
-
-        for _ in range(2000):
-            secret = evolve(secret)
-            price = secret % 10
-            change = price - prev_price
-
-            # Find the sequence
-            if change == sequence[pointer]:
-                pointer += 1
-                if pointer >= len(sequence):
-                    offers.append( price )
-                    pointer = 0
-            else:
-                pointer = 0
-
-            prev_price = price
-
-        if len(offers)> 0:
-            result.append( max(offers ))
-
-
-    return sum(result)
-
-# Find all 4 sequences
-def find_sequences( secrets ):
-
-    result = []
-
-    seq = deque()
-
-    for secret in secrets:
-        prev_price = secret % 10
-        for _ in range(2000):
-            secret = evolve(secret)
-            price = secret % 10
-            change = price - prev_price
-
-            seq.append(change)
-
-            if len(seq) > 4:
-                seq.popleft()
-
-            if len(seq) == 4 and list(seq) not in result:
-                result.append(tuple(seq))
-
-            prev_price = price
-
-    return result
-
-sequences = find_sequences(numbers)
-
 part2 = 0
-for i in range( len(sequences) - 1 ):
-    percent = int((i * 100 ) / (len(sequences) - 1 ))
-    if percent % 2 ==  0:
-        print(f"{percent} %")
 
-    offer = get_offer( numbers, tuple(sequences[i]))
-    if offer > part2:
-        part2 = offer
+for seq in unique_sequences:
 
-print("Part 2:", part2)
+    seq_best_offer = 0
+
+    for number in numbers:
+        if number in sequence_map:
+            if seq in sequence_map[number]:
+                seq_best_offer += sequence_map[number][seq]
+
+    if seq_best_offer > part2:
+        part2 = seq_best_offer
+        print("Best" , part2 , seq )
+
+print("Part 2:", part2 )
