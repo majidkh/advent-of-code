46f3d1d5da63dd17bccb82a0024122388723b909
Majid Khosravi <majid@Majids-MacBook-Air.local>
2024-12-24 18:40:53 +0000

    Day 20 Part 1 fixed

--- 20.py
+++ 20.py
@@ -1,76 +1,81 @@
 from collections import deque
-from functools import cache
-import copy
 
 f = open("inputs/20.txt", "r")
 lines = f.read().splitlines()
 f.close()
-track = tuple(tuple(tuple(line)) for line in lines)
-width = len(track[0]) # Add one extra
+track = [list(line) for line in lines]
+width = len(track[0])
 height = len(track)
-cheats = []
+distances = {}
 
-def get_cell ( col , row ):
+def get_cell ( input_map, col , row ):
     if col < 0 or col >= width or row < 0 or row >= height:
         return None
-    return track[row][col]
-
-for cy in range( 1 , height - 1):
-    for cx in range(width):
-        cheats.append((cx,cy))
+    return input_map[row][col]
 
 def find_symbol( symbol ):
-
     for col in range(width):
         for row in range(height):
             if track[row][col] == symbol:
                 return col, row
-
     return None, None
+
+def pretty_print( input_map ):
+    output = ""
+    for i in range(width):
+        for j in range(height):
+            output += str(input_map[i][j]).center(4 , " ")
+        output += "\033[0m\n"
+    print(output)
 
 start = find_symbol("S")
 end = find_symbol("E")
 
-@cache
-def find_path( cheat , cheat_length ):
+def mark_path ( input_map , end ):
 
-    path = deque([( start[0],start[1],0)]) # Start Position
-    path_cache = {(0,0)}
+    output = [[-1 for _ in range(height)] for _ in range(width)]
 
-    while path:
-        col,row,distance = path.popleft()
+    output[end[1]][end[0]] = 0
 
-        for i in [ (1,0) , (-1,0), (0,1), (0,-1) ]:
-            x, y = ( col + i[0], row + i[1] )
+    seen = set()
+    seen.add(end)
+    queue = deque([end])
+    while queue:
+        x,y = queue.popleft()
+        for nx,ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
 
-            if x < 0 or x >= width or y < 0 or y >= height: continue
+            if nx < 0 or nx >= width or ny < 0 or ny >= height: continue
 
-            if track[y][x] == "#":
+            if (nx,ny) in seen: continue
 
-                # Check cheat
-                if cheat_length == 1 and cheat != (x,y): continue
+            if input_map[ny][nx] == "." or input_map[ny][nx] == "S":
+                output[ny][nx] = output[y][x] + 1
+                seen.add((nx,ny))
+                queue.append((nx,ny))
+    return output
 
+def is_shortcuts ( input_map , x , y , target_distance, radius ):
+    values = []
+    for nx, ny in [( x + 1, y ), (x - 1, y), (x, y + 1), (x, y - 1)]:
+        if nx < 0 or nx >= width or ny < 0 or ny >= height: continue
+        value = get_cell(input_map, nx, ny)
+        if value > -1:
+            values.append ( value )
 
-            if (x,y) in path_cache: continue
+    if len(values) > 1:
+        if max(values) - min(values) > target_distance:
+            return True
 
-            if (x,y) == end:
-                return distance +1
+    return False
 
-            path_cache.add((x,y))
-            path.append( (x,y , distance + 1) )
+def find_cheats ( input_map , time_save , radius = 1 ):
+    cheats = 0
+    for col in range(width):
+        for row in range(height):
+            if input_map[row][col] == -1:
+                if is_shortcuts( input_map, col, row, time_save + 1, radius):
+                    cheats += 1
+    return cheats
 
-    return None
-
-normal_time = find_path( (-1,-1) , 1 )
-
-
-part1 = 0
-print(len(cheats))
-for i in range(len(cheats)):
-    c = cheats[i]
-    result = find_path( c , 20 )
-    if result is not None and result < normal_time:
-        if normal_time  - result >= 100:
-            part1 += 1
-
-print("Part 1:", part1)
+distance_map = mark_path( track, end )
+print ( "Part 1:", find_cheats( distance_map  ,100 , 1 ))
