17f18ab40ae9487fba2fe9177b983c1d4e5139e6
Majid Khosravi <majid@Majids-MacBook-Air.local>
2024-12-24 22:10:54 +0000

    day 20 part 2 fixed

--- 20.py
+++ 20.py
@@ -31,51 +31,52 @@ def pretty_print( input_map ):
 start = find_symbol("S")
 end = find_symbol("E")
 
-def mark_path ( input_map , end ):
+def mark_path ( input_map , start ):
 
     output = [[-1 for _ in range(height)] for _ in range(width)]
-
-    output[end[1]][end[0]] = 0
-
     seen = set()
-    seen.add(end)
-    queue = deque([end])
+    output [start[1]] [start[0]] = 0
+    seen.add(start)
+    queue = deque([start])
     while queue:
         x,y = queue.popleft()
         for nx,ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
 
             if nx < 0 or nx >= width or ny < 0 or ny >= height: continue
-
             if (nx,ny) in seen: continue
 
-            if input_map[ny][nx] == "." or input_map[ny][nx] == "S":
+            if input_map[ny][nx] == "." or input_map[ny][nx] == "E":
                 output[ny][nx] = output[y][x] + 1
                 seen.add((nx,ny))
                 queue.append((nx,ny))
     return output
 
-def is_shortcuts ( input_map , x , y , target_distance, radius ):
-    values = []
-    for nx, ny in [( x + 1, y ), (x - 1, y), (x, y + 1), (x, y - 1)]:
-        if nx < 0 or nx >= width or ny < 0 or ny >= height: continue
-        value = get_cell(input_map, nx, ny)
-        if value > -1:
-            values.append ( value )
+def find_cheats( radius , savings ):
 
-    if len(values) > 1:
-        if max(values) - min(values) > target_distance:
-            return True
-
-    return False
-
-def find_cheats ( input_map , time_save , radius = 1 ):
     cheats = 0
+    seen = set()
+
     for col in range(width):
         for row in range(height):
-            if input_map[row][col] == -1:
-                if is_shortcuts( input_map, col, row, time_save + 1, radius):
-                    cheats += 1
+            if distance_map[row][col] == -1: continue
+
+            for dx in range(-radius, radius + 1):
+                for dy in range(-radius, radius + 1):
+                    if abs(dx) + abs(dy) <= radius:
+                        nx = col + dx
+                        ny = row + dy
+
+                        if (nx,ny) in seen: continue
+                        if nx < 0 or nx >= width or ny < 0 or ny >= height: continue
+                        if distance_map[ny][nx] == -1: continue
+                        margin = abs(col - nx ) + abs(row - ny)
+
+                        jump = distance_map[row][col] - distance_map[ny][nx] - margin
+
+                        if jump >= savings :
+                            cheats += 1
     return cheats
 
-distance_map = mark_path( track, end )
-print ( "Part 1:", find_cheats( distance_map  ,100 , 1 ))
+distance_map = mark_path( track, start )
+print ("part 1:", find_cheats(2 , 100 ) )
+print ("part 2:", find_cheats(20 , 100 ) )
