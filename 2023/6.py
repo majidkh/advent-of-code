cc0fbb0ea9e50e646640eae808e30be05629027f
Majid Khosravi <me@majidkhosravi.com>
2024-12-29 12:07:52 +0000

    Day 6 2023

---
+++ 2023/6.py
@@ -0,0 +1,30 @@
+import re
+
+f = open("inputs/6.txt", "r")
+times = []
+distances = []
+
+for line in f.read().splitlines():
+    if line.startswith("Time"):
+        times = list(re.findall(r"\d+", line))
+    elif line.startswith("Distance"):
+        distances = list(re.findall(r"\d+", line))
+
+
+def calculate_ways(time, record):
+    ways = 0
+    for t in range(time + 1):
+        speed = t
+        distance = speed * (time - t)
+        if distance > record:
+            ways += 1
+    return ways
+
+
+part1 = 1
+for i in range(len(times)):
+    part1 *= calculate_ways(int(times[i]), int(distances[i]))
+print("Part1:", part1)
+
+part2 = calculate_ways(int(''.join(times)), int(''.join(distances)))
+print("Part2:", part2)
