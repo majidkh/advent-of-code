24bdaec34e38180a481e4177b1e3f41659dd5389
Majid Khosravi <majid@Majids-MacBook-Air.local>
2024-12-29 23:15:02 +0000

    Day 7 part 2

--- 2023/7.py
+++ 2023/7.py
@@ -1,5 +1,6 @@
 import re
 from functools import cmp_to_key
+from itertools import product
 
 f = open("inputs/7.txt", "r")
 lines = f.read().splitlines()
@@ -10,18 +11,42 @@ for line in lines:
     hands.append((line[:5], int(list(re.findall("(\d+)", line[6:]))[0])))
 
 
-def get_hand_type(hand, use_joker=False):
+def find_best_joker_hand(hand):
+    best_hand = hand
+
+    if hand.count("J") == 0: return best_hand
+    cards = list(hand)
+
+    unique = list({c for c in cards if c != 'J'})
+    jokers = [i for i, v in enumerate(cards) if v == 'J']
+    combinations = list(product(unique, repeat=len(jokers)))
+
+    best_score = 0
+
+    for combo in combinations:
+        new_list = list(cards)
+        for idx, rep in zip(jokers, combo):
+            new_list[idx] = rep
+
+        new_hand = ''.join(new_list)
+        score = get_hand_score(new_hand)
+        if score > best_score:
+            best_score = score
+            best_hand = new_hand
+        elif score == best_score:
+            if compare_cards(best_hand, new_hand, labels2):
+                best_hand = new_hand
+
+    return best_hand
+
+
+def get_hand_score(str_hand):
     cards = {}
-    for card in hand[0]:
+    for card in str_hand:
         if card in cards:
             cards[card] += 1
         else:
             cards[card] = 1
-
-    if use_joker:
-        jokers = hand[0].count("J")
-        if jokers > 0:
-            print(hand) # TODO
 
     cards = sorted(cards.values(), reverse=True)
 
@@ -85,7 +110,7 @@ def compare_part_1(hand1, hand2):
 deck = []
 
 for h in hands:
-    deck.append((h, get_hand_type(h)))
+    deck.append((h, get_hand_score(h[0])))
 deck.sort(key=cmp_to_key(compare_part_1))
 
 part1 = 0
@@ -93,3 +118,26 @@ for i in range(len(deck)):
     h = deck[i]
     part1 += (i + 1) * h[0][1]
 print("Part 1:", part1)
+
+
+# Part 2
+def compare_part_2(hand1, hand2):
+    if hand1[1] > hand2[1]:
+        return 1
+    elif hand1[1] < hand2[1]:
+        return -1
+    return compare_cards(hand1[0][0], hand2[0][0], labels2)
+
+
+deck = []
+
+for h in hands:
+    swapped = find_best_joker_hand(h[0])
+    deck.append((h, get_hand_score(swapped)))
+deck.sort(key=cmp_to_key(compare_part_2))
+
+part2 = 0
+for i in range(len(deck)):
+    h = deck[i]
+    part2 += (i + 1) * h[0][1]
+print("Part 2:", part2)
