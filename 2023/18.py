796fd32dd8aa98c72572d5ba69ee3b3860931da1
Majid <majid@Majids-MacBook-Air.local>
2025-01-30 20:12:28 +0000

    Day 18 part 2

--- 2023/18.py
+++ 2023/18.py
@@ -1,4 +1,3 @@
-import math
 import re
 
 f = open('inputs/18.txt', 'r')
@@ -15,110 +14,29 @@ for line in lines:
     instructions.append( (data[0][0], int(data[0][1])) )
     instructions2.append( (num_dirs[data[0][2][-1]], int(data[0][2][:-1],16)) )
 
+def shoelace_area(points):
+    n = len(points)
+    area = 0
+    for i in range(n):
+        x1, y1 = points[i]
+        x2, y2 = points[(i + 1) % n]
+        area += x1 * y2 - x2 * y1
+    return int(abs(area) / 2)
 
-# find minimum x , y so later we can adjust the grid
-x, y = 0, 0
-min_x = min_y = 0
-for instruction in instructions:
-    direction, length = instruction
-    d = dirs[direction]
-    for i in range(length):
-        x += d[0]
-        y += d[1]
-        if x < min_x: min_x = x
-        if y < min_y: min_y = y
+def calculate( input_ ):
+    x,y = (0,0)
+    border = 0
+    plans = [ (x,y)]
+    for instruction in input_:
+        direction, length = instruction
+        d = dirs[direction]
+        x +=  d[0] * length
+        y +=  d[1] * length
+        border += length
+        plans.append( (x, y) )
+    area = shoelace_area(plans)
+    inside = area - border // 2 + 1
+    return inside+border
 
-# Add one row and column margin from left and top for bfs search
-min_x -= 1
-min_y -= 1
-
-# create the dig plans
-plans = set()
-x, y = 0, 0
-for instruction in instructions:
-    direction, length = instruction
-    d = dirs[direction]
-    for i in range(length):
-        x += d[0]
-        y += d[1]
-        plans.add((x + abs(min_x), y + abs(min_y)))
-
-# add one margin to right and bottom too
-max_x = max(pt[0] for pt in plans) + 1
-max_y = max(pt[1] for pt in plans) + 1
-
-width = max_x - min_x
-height = max_y - min_y
-
-grid = [["." for _ in range(width)] for _ in range(height)]
-
-# Used to caching the below function so we don't search all the cells again
-open_list = set()
-closed_list = set()
-
-def is_closed(x1, y1):
-    global open_list, closed_list
-
-    if (x1, y1) in open_list:
-        return False
-    elif (x1, y1) in closed_list:
-        return True
-
-    if x1 == 0 or x1 == width - 1 or y1 == 0 or y1 == height - 1:
-        return False
-
-    q = [(x1, y1)]
-    seen = set()
-
-    corners = {(0, 0), (width - 2, 0), (0, height - 2), (width - 2, height - 2)}
-
-    enclosed = True
-
-    while q:
-        x1, y1 = q.pop()
-
-        for nx, ny in [(x1 + 1, y1), (x1 - 1, y1), (x1, y1 + 1), (x1, y1 - 1)]:
-            if nx < 0 or nx >= width or ny < 0 or ny >= height: continue
-            if (nx, ny) in plans: continue
-
-            if (nx, ny) in seen: continue
-            seen.add((nx, ny))
-
-            if (nx, ny) in corners:
-                enclosed = False
-
-            q.append((nx, ny))
-
-    if not enclosed:
-        open_list |= seen
-    else:
-        closed_list |= seen
-
-    return enclosed
-
-
-part1 = 0
-for j in range(height):
-    for i in range(width):
-        if (i, j) in plans:
-            grid[j][i] = "#"
-            part1 += 1
-        else:
-            # check if this block is within the shape
-            if is_closed(i, j):
-                grid[j][i] = "#"
-                part1 += 1
-
-
-def pretty_print(g):
-    output = ""
-    for row in range(height):
-        for col in range(width):
-            output += str(g[row][col])
-        output += "\n"
-    print(output)
-
-
-# pretty_print(grid)
-
-print("Part 1:", part1)
+print("Part 1:", calculate(instructions))
+print("Part 2:", calculate(instructions2))
