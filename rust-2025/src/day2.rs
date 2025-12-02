59ed7137b2d4a4a2b5dbe570dedd134570e9f9cf
Majid <majid@Majids-MacBook-Air.local>
2025-12-02 18:22:18 +0000

    Day 2 part 2 - 2025

--- rust-2025/src/day2.rs
+++ rust-2025/src/day2.rs
@@ -10,33 +10,57 @@ pub fn part1() -> i64
             let first: i64 = parts[0].parse().expect("Failed to parse first number");
             let second: i64 = parts[1].parse().expect("Failed to parse second number");
 
-            invalids += find_invalids(first, second);
+            invalids += find_invalids(first, second , false );
         }
     }
     invalids
 }
 
-pub fn part2() -> i32
+pub fn part2() -> i64
 {
-    0
+    let mut invalids = 0;
+    if let Ok(contents) = read_contents("src/input/2.txt") {
+        let text = contents.replace("\r", " ").replace("\n", "");
+        let ranges: Vec<&str> = text.split(',').collect();
+        for range in ranges {
+            let parts:Vec<&str> = range.split("-").collect();
+            let first: i64 = parts[0].parse().expect("Failed to parse first number");
+            let second: i64 = parts[1].parse().expect("Failed to parse second number");
+
+            invalids += find_invalids(first, second , true );
+        }
+    }
+    invalids
 }
 
-fn find_invalids( from :i64 , to:i64 ) -> i64
+fn find_invalids( from :i64 , to:i64, repeat:bool ) -> i64
 {
     let mut c = 0;
     for i in from..=to {
         let str_i = i.to_string();
         let str_len = str_i.len();
-        if str_len % 2 == 0{
-
-            let first_half: String = str_i.chars().take(str_len / 2).collect();
-            let second_half: String = str_i.chars().skip(str_len / 2).collect();
-
-            if first_half == second_half {
-                c +=i;
+        if repeat {
+            if is_invalid(str_i , 0 , str_len/2) {
+                c += i
+            }
+        }
+        else {
+            if str_len % 2 == 0 && is_invalid(str_i , (str_len/2)-1 , str_len/2 ) {
+                c += i
             }
         }
     }
-
     c
+}
+
+fn is_invalid ( number: String , from: usize , to: usize ) -> bool
+{
+    let str_len = number.len();
+    for i in from..to{
+        let r = number.chars().take(i + 1).collect::<String>().repeat(str_len/(i+1) );
+        if r == number{
+            return true
+        }
+    }
+    false
 }
