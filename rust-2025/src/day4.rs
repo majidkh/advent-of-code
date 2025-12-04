e27303eb442d65238a7b96fffce60e36ce0f99b6
Majid <majid@Majids-MacBook-Air.local>
2025-12-04 18:08:15 +0000

    Day 4 part 1

---
+++ rust-2025/src/day4.rs
@@ -0,0 +1,77 @@
+use crate::helper::*;
+
+pub fn part1() -> i32
+{
+    let mut counter = 0;
+
+    if let Ok(lines) = read_lines("src/input/4.txt") {
+
+        let grid: Vec<Vec<char>> = lines
+            .filter_map(Result::ok)       // ignore lines that failed to read
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let grid_copy = grid.clone();
+
+        for (i,row) in grid.iter().enumerate() {
+            for (j, _value) in row.iter().enumerate() {
+                if grid[i][j] == '@' {
+                    if count(&grid_copy, i as i32, j as i32) < 4
+                    {
+                        counter += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    counter
+}
+
+pub fn part2() -> i32
+{
+    0
+}
+
+
+fn count ( grid : &Vec<Vec<char>>, row: i32, col : i32 ) -> i32
+{
+    let adjacent: Vec<(isize, isize)> = vec![
+        (-1, -1), // top-left
+        (0, -1),  // top
+        (1, -1),  // top-right
+        (-1, 0),  // left
+        (1, 0),   // right
+        (-1, 1),  // bottom-left
+        (0, 1),   // bottom
+        (1, 1),   // bottom-right
+    ];
+
+    let mut count = 0;
+
+    for pos in adjacent
+    {
+        if has_paper(grid , row + pos.0 as i32 , col + pos.1 as i32 )
+        {
+            count += 1;
+        }
+
+    }
+
+    count
+}
+
+fn has_paper( grid : &Vec<Vec<char>>, row: i32, col :i32 )-> bool
+{
+    if col < 0 || row < 0
+    {
+        return false;
+    }
+
+    if col >= grid.len() as i32 || row >= grid[0].len() as i32
+    {
+        return false;
+    }
+
+    if grid[row as usize][col as usize] == '@' { true } else { false }
+}
--- rust-2025/src/main.rs
+++ rust-2025/src/main.rs
@@ -2,9 +2,11 @@ mod helper;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("Day 1 - Part 1: {} , Part 2: {}",day1::part1(), day1::part2());
     println!("Day 2 - Part 1: {} , Part 2: {}",day2::part1(), day2::part2());
     println!("Day 3 - Part 1: {} , Part 2: {}",day3::part1(), day3::part2());
+    println!("Day 4 - Part 1: {} , Part 2: {}",day4::part1(), day4::part2());
 }
