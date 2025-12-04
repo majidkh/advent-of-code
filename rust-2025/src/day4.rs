cf49ef5e9db8c2866838e61f8a420af7c9437d1d
Majid <majid@Majids-MacBook-Air.local>
2025-12-04 18:31:47 +0000

    Day 4 part 2

--- rust-2025/src/day4.rs
+++ rust-2025/src/day4.rs
@@ -30,7 +30,37 @@ pub fn part1() -> i32
 
 pub fn part2() -> i32
 {
-    0
+    let mut counter = 0;
+
+    if let Ok(lines) = read_lines("src/input/4.txt") {
+
+        let grid: Vec<Vec<char>> = lines
+            .filter_map(Result::ok)       // ignore lines that failed to read
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let mut grid_copy = grid.clone();
+        let mut found = true;
+
+        while found
+        {
+            found = false;
+            for (i, row) in grid.iter().enumerate() {
+                for (j, _value) in row.iter().enumerate() {
+                    if grid_copy[i][j] == '@' {
+                        if count(&grid_copy, i as i32, j as i32) < 4
+                        {
+                            counter += 1;
+                            found = true;
+                            grid_copy[i][j] = 'x';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    counter
 }
 
 
@@ -68,7 +98,7 @@ fn has_paper( grid : &Vec<Vec<char>>, row: i32, col :i32 )-> bool
         return false;
     }
 
-    if col >= grid.len() as i32 || row >= grid[0].len() as i32
+    if row >= grid.len() as i32 || col >= grid[0].len() as i32
     {
         return false;
     }
