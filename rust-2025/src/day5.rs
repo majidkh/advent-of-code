5b105901af3ea39b3f1bda93c92e8300104bf159
Majid <majid@Majids-MacBook-Pro.local>
2025-12-05 12:57:19 +0000

    Day 5 part 2

--- rust-2025/src/day5.rs
+++ rust-2025/src/day5.rs
@@ -35,14 +35,65 @@ pub fn part1() -> i32
     fresh
 }
 
-pub fn part2() -> i32
+pub fn part2() -> u64
 {
-    0
+    let mut fresh_ids = 0;
+
+    let mut ranges: Vec<[u64; 2]> = Vec::new();
+    if let Ok(lines) = read_lines("src/input/5.txt") {
+        for line in lines {
+            if let Ok(text) = line {
+                if text.len() > 0 {
+                    // Ranges
+                    if text.contains('-') {
+                        let mut parts: Vec<u64> = text
+                            .split('-')
+                            .map(|s| s.parse::<u64>().unwrap())
+                            .collect();
+
+                        let mut new_parts = parts.clone();
+                        let mut split = false;
+                        // Count unique ranges
+                        for  &[r1, r2] in ranges.iter()
+                        {
+                            if parts[0] >= r1 && parts[0] <= r2 {
+                                parts[0] = r2 + 1
+                            }
+                            if parts[1] >= r1 && parts[1] <= r2 {
+                                parts[1] = r1 - 1;
+                            }
+
+                            // if needs splitting
+                            if parts[0] < r1 && parts[1] > r2 {
+                                new_parts[0] = r2 + 1;
+
+                                parts[1] = r1 - 1;
+                                split = true;
+                            }
+                        }
+
+                        if parts[1] >= parts[0] {
+                            ranges.push([parts[0], parts[1]]);
+                            if split{
+                                ranges.push([new_parts[0], new_parts[1]]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for  &[ n1, n2] in ranges.iter(){
+        fresh_ids += n2 - n1 + 1;
+    }
+
+    fresh_ids
 }
 
 fn is_fresh (id: u64, range: &Vec<[u64; 2]>) -> bool
 {
-    for (i, &[r1, r2]) in range.iter().enumerate() {
+    for &[r1, r2] in range.iter() {
         if id >= r1 && id <= r2 {
             return true
         }
--- rust-2025/src/main.rs
+++ rust-2025/src/main.rs
@@ -6,11 +6,9 @@ mod day4;
 mod day5;
 
 fn main() {
-    /*
     println!("Day 1 - Part 1: {} , Part 2: {}",day1::part1(), day1::part2());
     println!("Day 2 - Part 1: {} , Part 2: {}",day2::part1(), day2::part2());
     println!("Day 3 - Part 1: {} , Part 2: {}",day3::part1(), day3::part2());
     println!("Day 4 - Part 1: {} , Part 2: {}",day4::part1(), day4::part2());
-     */
     println!("Day 5 - Part 1: {} , Part 2: {}",day5::part1(), day5::part2());
 }
