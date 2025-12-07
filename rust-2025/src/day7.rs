f87750a42146bbfedb936fe813a5315c6dbc3163
Majid <majid@Majids-MacBook-Pro.local>
2025-12-07 13:08:16 +0000

    Day 7 part 2

--- rust-2025/src/day7.rs
+++ rust-2025/src/day7.rs
@@ -1,5 +1,5 @@
+use std::collections::{HashMap};
 use crate::helper::*;
-
 pub fn part1() -> usize
 {
     let grid = read_grid();
@@ -11,13 +11,7 @@ pub fn part1() -> usize
         for (col,loc) in line.iter().enumerate() {
             if *loc == 'S' || manifold[row][col] == '|'
             {
-                move_beam(col,row , & mut manifold , & mut split);
-
-                if row < grid.len() -1{
-                    if manifold[row+1][col] == '.'{
-                        manifold[row+1][col] = '|';
-                    }
-                }
+                move_beam(col,row , & mut manifold , & mut split  );
             }
         }
     }
@@ -25,9 +19,23 @@ pub fn part1() -> usize
     split
 }
 
-pub fn part2() -> u64
+pub fn part2() -> i64
 {
-    0
+    let mut cache:HashMap<(usize, usize), i64> = HashMap::new();
+    let grid = read_grid();
+    let mut timelines = 0;
+
+    for (row,line) in grid.iter().enumerate()
+    {
+        for (col,loc) in line.iter().enumerate() {
+            if *loc == 'S' || grid[row][col] == '|'
+            {
+                timelines = count_timelines(col,row , & grid , & mut cache );
+            }
+        }
+    }
+
+    timelines
 }
 
 fn read_grid() -> Vec<Vec<char>>
@@ -37,11 +45,9 @@ fn read_grid() -> Vec<Vec<char>>
         for line in lines {
             if let Ok(text) = line {
                 grid.push(text.chars().collect());
-
             }
         }
     }
-
     grid
 }
 
@@ -53,13 +59,37 @@ fn move_beam( col:usize , row:usize , manifold:&mut Vec<Vec<char>> , split: &mut
             move_beam( col ,row +1 ,manifold , split );
         }
         if manifold[row+1][col] == '^'{
-            if row < manifold.len()-2{
-                if manifold[row+2][col-1] == '.' || manifold[row+2][col+1] == '.'{
-                    *split += 1;
-                }
+            if manifold[row+2][col-1] == '.' || manifold[row+2][col+1] == '.'{
+                *split += 1;
             }
-            move_beam( col -1 ,row +1 ,manifold , split );
-            move_beam( col +1 ,row +1 ,manifold , split );
+            move_beam( col -1 ,row +1 ,manifold , split  );
+            move_beam( col +1 ,row +1 ,manifold , split  );
         }
     }
+}
+
+fn count_timelines( col:usize , row:usize , grid: &Vec<Vec<char>> , cache: &mut HashMap<(usize,usize),i64> ) -> i64
+{
+    let mut timelines: i64 = 0;
+
+    if let Some(value) = cache.get(&(col,row)) {
+        return *value;
+    }
+
+    if row < grid.len() -1{
+        if grid[row + 1][col] == '.' {
+            timelines += count_timelines(col, row + 1, grid, cache);
+        }
+        else if grid[row + 1][col] == '^'
+        {
+            timelines += count_timelines(col - 1, row + 2, grid, cache );
+            timelines += count_timelines(col + 1, row + 2, grid, cache );
+        }
+    }
+    else {
+        timelines += 1;
+
+    }
+    cache.insert((col,row),timelines);
+    timelines
 }
